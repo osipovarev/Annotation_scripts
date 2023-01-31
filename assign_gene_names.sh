b84c3300ab49eccd9f04dcc780d2d34451166d7a
Ekaterina Osipova <eosipova@boslogin01.rc.fas.harvard.edu>
2023-01-31 15:54:18 -0500

    removed .version removal

--- assign_gene_names.sh
+++ assign_gene_names.sh
@@ -41,7 +41,8 @@ fi
 
 echo -e "Running overlapSelect....."
 echo -e "NB: using 80% CDS overlap requirement"
-overlapSelect -statsOutput -strand -inCds -overlapThreshold=0.8 -inFmt=bed -selectFmt=bed $NAMEDANNO $ANNOBED stdout | cut -f1-2 | rev | cut -d"." -f2- | rev | awk '{print $2"\t"$1}' > $ANNODICT
+#overlapSelect -statsOutput -strand -inCds -overlapThreshold=0.8 -inFmt=bed -selectFmt=bed $NAMEDANNO $ANNOBED stdout | cut -f1-2 | rev | cut -d"." -f2- | rev | awk '{print $2"\t"$1}' > $ANNODICT
+overlapSelect -statsOutput -strand -inCds -overlapThreshold=0.8 -inFmt=bed -selectFmt=bed $NAMEDANNO $ANNOBED stdout | cut -f1-2 |  awk '{print $2"\t"$1}'  > $ANNODICT
 
 echo -e "Running renamig using replace_names_from_dict.py....."
 replace_names_from_dict.py -f 1 -a $ANNODICT -d <(awk '{print $2","$1}' $NAMESFILE) | grep -v  ^reg_ | grep -v ^rna- | awk '{print $2","$1}' | sort -u > $NAMESDICT
@@ -50,6 +51,6 @@ echo -e "Giving geneName_ prefix to each transcript....."
 replace_names_from_dict.py -a $ANNOBED -d $NAMESDICT -p  > $OUTFILE
 
 echo -e "Cleaning up....."
-rm $ANNODICT $NAMESDICT
+#rm $ANNODICT $NAMESDICT
 
 echo -e "All Done! Check $OUTFILE"
